d57dcdc75b2c982aa757fefb56fe9364658263bd
castilloubedaruben <castilloubedaruben001.alu@stemgranada.com>
2026-03-11 13:49:42 +0100

    Proyecto maven para probar test

---
+++ practicaTestCalculadora/src/main/java/es/stemgranada/practicatestcalculadora/PracticaTestCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package es.stemgranada.practicatestcalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaTestCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaTestCalculadoraApplication.class, args);
+	}
+
+}
---
+++ practicaTestCalculadora/src/test/java/es/stemgranada/practicaTestCalculadora/PracticaTestCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package es.stemgranada.practicaTestCalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaTestCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
